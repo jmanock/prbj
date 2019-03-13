f52fdc5df665bf366c38837225434dc6a5c4df98
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-03-13 07:10:48 -0400

    about section added

--- src/Components/Layouts/About.jsx
+++ src/Components/Layouts/About.jsx
@@ -2,7 +2,56 @@ import React from 'react';
 
 const About = () =>{
   return(
-    <h1>About</h1>
+    <div className='container'>
+      <div className='card'>
+        <div className='card-body'>
+          <h5 className='card-title text-center'>Once upon a time...</h5>
+          <p className='card-text'>1986 to be exact ~ Tim and Judy decided to go down to the local animal shelter to help out with an adoption event. Throughout the day Tim worked and worked to try to get someone to adopt an Old English Sheepdog that had caught his eye. Sadly, at the end of the day, that sheepdog still sat there, unloved and alone. When Judy ventured to say that she'd like to adopt a doberman she'd seen, Tim said no, they were taking the sheepdog. You see, he knew that, because it was older, it would soon be euthanized. Well, that dog lived another 8 years, and Judy's involvement in animal resue grew into passion and eventually an organization.</p>
+        </div>
+      </div>
+      <div className='card'>
+        <div className='card-body'>
+          <h5 className='card-title text-center'>Who We Are Today</h5>
+          <p className='card-text'>Pet Rescue By Judy is a non-profit 501(c)(3) non profit "NO KILL" organization dedicated to the Rescuing ~ Rehabilitation ~ Resocializing and Rehoming of Companion Animals. Many of them come to us with medical needs. They are doctored to the best of our ability and resources.</p>
+        </div>
+      </div>
+      <div className='card'>
+        <div className='card-body'>
+          <h5 className='card-title text-center'>Our Goals</h5>
+          <p className='card-text'>Obviously our most urgent goal is to resue animals that are abandoned or in distress, update their vaccines, and find good new homes for them. But we attempt to be more proactive than that. It is our heartfelt desire to prevent any animal from the need to be "rescued." With that vision in mind, we take a three-prong approach through the education of pet owners and general public about:</p>
+          <ul className='list-group list-group-flush'>
+            <li className='list-group-item'>proper care of animals, including correct diet of good quality food, fresh clean water, daily exercise, and lots of love</li>
+            <li className='list-group-item'>spaying and neutering companion animals (dog and cats) in order to prevent euthanasia and over-population.</li>
+            <li className='list-group-item'>low-cost spay/neuter clinics and wellness centers in their local community.</li>
+          </ul>
+        </div>
+      </div>
+      <div className='card'>
+        <div className='card-body'>
+          <h5 className='card-title text-center'>It Takes More Than Good Intentions</h5>
+          <p className='card-text'>Many of the animals we receive arrive from less than ideal situations. We want to help them recover their health while not risking the health of our other animals, and stop the cycle of breeding unwanted pets. With that in mind, each rescued animal undergoes medical and screening procedures:</p>
+          <ul className='list-group list-group-flush'>
+            <li className='list-group-item'>Each animal is examined for immediate medical problems and has its ears cleaned and nails trimmed.</li>
+            <li className='list-group-item'>All animals are bathed, immediately administered flea/tick preventative, dewormed and tested for heatworm.</li>
+            <li className='list-group-item'>Shots are updated, including a rabies vaccination.</li>
+            <li className='list-group-item'>All pets are micro-chipped using the HomeAgain microchip system.</li>
+            <li className='list-group-item'>Cats are tested for feline leukemial/AIDS</li>
+          </ul>
+        </div>
+      </div>
+      <div className='card'>
+        <div className='card-body'>
+          <h5 className='card-title text-center'>Adiptiong A Friend</h5>
+          <p className='card-text'>Many, if not most, of our pets are in foster homes, therefore to set up an appointment to meet a specific pet, please complete an online adoption application.</p>
+        </div>
+      </div>
+      <div className='card'>
+        <div className='card-body'>
+          <h5 className='card-title text-center'>We Need Your Help</h5>
+          <p className='card-text'>Would you like to help? We are always in need of Foster Homes. Fostering for a week or a month can make a difference in an animal's life. Also, we welcome help with walking, feeding, cleaning, adoptions, paperwork, photography - whenever you have time to give, we appreciate you. Apply to be a Volunteer! And, of course, we are always in need of donations - money, food, crates, beds etc. You can even come to the aid of a specific animal through our Sponsorship Program. Thank You!</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
