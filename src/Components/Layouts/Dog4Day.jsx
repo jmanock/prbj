f0cd89f8a0675545efdabb90f8a3692708bad603
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-03-14 07:58:52 -0400

    added new page

--- src/Components/Layouts/Dog4Day.jsx
+++ src/Components/Layouts/Dog4Day.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
 const Dog4Day = () =>(
-  <h1>Dog4Day</h1>
+  <div className='container'>
+    <h3>Thank you for your interest in our Dog 4 a Day Program!</h3>
+    <small>This program is available daily from 10:00am - 5:00pm</small>
+    <div className='card'>
+      <div className='card-body'>
+        <p className='card-text'>If interested, plese submit a dog for ad day application to make an appointment. If you've already filled out an application in the past and have participated in this program before, there is no need to re-apply!</p>
+        <p className='card-text'>Please email hannahprbj@gmail.com to make an appointment <br /> YOU MUST BE 18 YEARS OR OLDER TO PARTICIPATE</p>
+      </div>
+    </div>
+
+    <div className='card'>
+      <div className='card-body'>
+        <p className='card-text'>With Dog 4 a Day, you have the opportunity to spend a fun-filled day with our pooches by taking them our of their kennels and going on an outing of your choice. Some great things to do with you dog for the day are: taking walks, going for car rides, playing with toys, getting a special treat, snuggling with your pooch, and having a great time knowing you're giving your friendly new pal a day outside of the shelter! Please be aware some specific dogs have specific needs, and not all are available for this program; we also ask that you do NOT take them to dog parks on your outing</p>
+      </div>
+    </div>
+
+    <div className='card'>
+      <div className='card-body'>
+        <p className='card-text'>The outing can last for a few hours or for the day, which gives our pups a chance for some one-on-one attention and fun! Many people decide to take them home, so the dogs can experience some family and lounge time.</p>
+        <p className='card-text'>If you have any questions about the Dog 4 a Day program, please emailL hannahprbj@gmail.com</p>
+      </div>
+    </div>
+
+    <div className='card'>
+      <div className='card-body'>
+        <p className='card-text'>PLEASE NOTE: While we try to accommodate requests, we cannot guarantee which dogs will be available. Puppies under the age of 6 months are not available for this program, and dogs with an `F` by their name means they are currently in foster homes. To take a dog our for the day, you mush be 18 years of age or older (we do verify  this information at time of pick up)</p>
+        <p className='card-text'>If you are interested in ADOPTING one of our dogs, please submit an adoption application. The option to take them for the day before finalizing an adoption may be available and will be coordinated by one of our adoption volunteers. Submitting a D4D application is not necessary</p>
+      </div>
+    </div>
+  </div>
 );
 
 export default Dog4Day;
