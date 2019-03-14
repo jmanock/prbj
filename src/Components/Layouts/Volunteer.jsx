07a188c51e53d8a7c9001b0a70d85bb850c7583c
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-03-14 09:18:01 -0400

    content on more pages

--- src/Components/Layouts/Volunteer.jsx
+++ src/Components/Layouts/Volunteer.jsx
@@ -2,33 +2,23 @@ import React from 'react';
 
 const Volunteer = () =>(
   <div className='container'>
+    <h3 className='text-center'>We Love Volunteers!</h3>
     <div className='card'>
       <div className='card-body'>
-        <p className='card-text'>Please Donate To Our Cause We are a non-profit 501(c)(3) organization Your donations are tax deductable PRBJ EIN#: 593297626</p>
+        <p className='card-text'>If you are already an approved volunteer, please click here or go to the Volunteer Portal on the left hand side of the website</p>
+        <p className='card-text'>Thank you for your interest in volunteering with PRBJ. If you would like to volunteer and are 16 years of age or older, please submit a volunteer application, which can be found here. We hope to work with you in the near future!</p>
+        <p classsName='card-text'>If you are volunteering for the UCF Leadership Class, please email volunteer@petrescuebyjudy.com to receive your orientation date. ATTN: UCF LEADERS</p>
       </div>
     </div>
-
     <div className='card'>
       <div className='card-body'>
-        <p className='card-text'>Monetary donations are always much needed and vary much appreciated, as are direct donations to our Veterinary partners which helps reduce the every present balance we maintain with them! Gift Cards to Pet Supply stores such as Petco or PetSmart are helpful as well. If you are interested in donating directly to our Shelter Renovation fund "Save the Tails" you may find donation information and options here. In addition to monetary donations, we have a wish list of needed items.. see below or NOW you can go directly to our Amazon Wish List!</p>
+        <p className='card-text'>Group Volunteering: This is GREAT for churches, companies, families, college groups, etc. Put your friends/co-workers/family to work. We will ask for a maximum of 15 volunteers per shift. (7-11, 11-3, 3-7) if you and your group would like to participate please submit a Goup Volunteer Application. Once approved you will receive an email from our volunteer coordinator. Each individual volunteer must print, sign, & submit all release forms back before the chosen volunteer date/dates.</p>
+        <p className='card-text text-center'>Click to go to and print Release/Hold Harmless Form</p>
       </div>
     </div>
-
     <div className='card'>
       <div className='card-body'>
-        <p className='card-text'>pet Rescue by Judy very much appreciates your donations! At this time, the items listed above are needed for our facility. We are grateful for the support of our community and thank you so much for thinking of us!</p>
-        <p className='card-text'>Donations, both big and small, are always welcome and appreciated. In Addition to our operating expenses, we also have expenses associated with the care of the pets. All of our pets need to be spayed/neutered, microchipped, brought up to date on shots, and treated for medical needs. Unfortunately, many times we also encounter unexpected medical expenses for many of our rescues. Although we have very supportive veterinarians, they cannot completely waive their fees.</p>
-        <p className='card-text'>Please help us if you can. You can make a one-time donation or choose to sponsor a specific pet. Perhaps you would consider committing to covering the cost of spaying/neutering one pet a month! When we know we can count on a force of people standing with us, what a difference we can make! Donations can be made through our Paypal account or by mailing a check. We are forever grateful for those who help us help them!</p>
-        <p className='card-text'>Your donations are tax deductible. All monies go directly to the care of the animals. Consider this: many large corporations have programs matching donations to nonprofit organizations. Ask your human resouce department. Make donations payable to:</p>
-        <p className='card-text text-center'>Pet Rescue By Judy <br /> 401 S. Laurel Avenue <br /> Sanford, FL 32771</p>
-        <small className='card-text'>Electronic donations may also be made safely and securely via the Paypal link below. Please include your mailing address on your Paypal donation so that we can be sure to mail you a tax receipt.</small>
-      </div>
-    </div>
-
-    <div className='card'>
-      <div className='card-body'>
-        <p className='card-text'>A COPY OF THE OFFICIAL REGISTRATION AND FINANCIAL INFORMATION MAY BE OBTAINED FROM THE DIVISION OF CONSUMER SERVICES BY CALLING TOLL-FREE, 800-435-7352 (800-HELP-FLA) WITHIN THE STATE. REGISTRATION DOES NOT IMPLY ENDORSEMENT, APPROVAL, OR RECOMMENDATION BY THE STATE. Floriday Registration #CH6649</p>
-        <p className='card-text text-center'>We are a non-profit 501(c)(3) organization <br /> Your donations are tax deductable</p>
+        <p className='card-text text-center'>For futher questions, please email our volunteer coordinator at volunteer@petrescuebyjudy.com</p>
       </div>
     </div>
   </div>
